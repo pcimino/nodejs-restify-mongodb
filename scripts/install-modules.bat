45f15b7f7d21ed6327dd4d8ed3b56a7f03dd5a14
Paul Cimino <pcimino@translunardesigns.com>
2013-04-24 18:29:53 -0400

    Update to include mail modules

--- scripts/install-modules.bat
+++ scripts/install-modules.bat
@@ -3,7 +3,4 @@ set SERVER_HOME=..
 
 cd %SERVER_HOME%
 
-call npm install restify
-call npm install mongo
-call npm install mongoose
-call npm install client-sessions
+call npm install 
--- scripts/install-modules.sh
+++ scripts/install-modules.sh
@@ -1,7 +1,4 @@
 # Assumes server is in parent directory, set to absolute path to run script from anywhere
 export SERVER_HOME=.. 
 cd $SERVER_HOME 
-npm install restify 
-npm install mongo 
-npm install mongoose 
-npm install client-sessions
+npm install 

64811fd21cc826e3ce3ff619bcb6735b710f3ef3
Paul L. Cimino <pcimino@translunardesigns.com>
2013-05-08 19:24:25 -0400

    Started building user signon flow

---
+++ models/verifycode.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId
+  , crypto = require('crypto')
+  , restify = require('restify');
+
+/**
+ * Verify Code is used to validate new accounts/email addresses
+ */
+
+var VerifyCodeSchema = new Schema({
+  id: ObjectId,
+  userObjectId: ObjectId,
+  key: { type: String, trim: true }
+})
+
+/**
+ * Validations
+ */
+
+var validatePresenceOf = function (value) {
+  return value && (value.length >= 12)
+}
+
+/**
+ * Pre-save hook
+ */
+
+VerifyCodeSchema.pre('save', function(next) {
+  if (!validatePresenceOf(this.key)) {
+    next(new restify.MissingParameterError('Invalid key'));
+  }
+  next();
+})
+
+
+mongoose.model('VerifyCode', VerifyCodeSchema)

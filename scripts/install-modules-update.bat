77f36f2773d368e3a38728c341bb19a8ef5acbff
Paul L. Cimino <pcimino@translunardesigns.com>
2013-06-27 19:24:25 -0400

    Added enyo.html

---
+++ scripts/install-modules-update.bat
@@ -0,0 +1,6 @@
+REM Assumes server is in parent directory, set to absolute path to run script from anywhere
+set SERVER_HOME=..
+
+cd %SERVER_HOME%
+
+call npm update 
---
+++ scripts/install-modules-update.sh
@@ -0,0 +1,4 @@
+# Assumes server is in parent directory, set to absolute path to run script from anywhere
+export SERVER_HOME=.. 
+cd $SERVER_HOME 
+npm update 

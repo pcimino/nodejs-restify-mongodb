8653da518fe25d51483a08cd021dba3a6d2ebd42
Paul L. Cimino <p.cimino@translunardesigns.com>
2014-01-24 16:31:57 -0500

    Adding should and superagent modules

--- test/service-routes-auth.js
+++ test/service-routes-auth.js
@@ -1,3 +1,9 @@
+
+// used for auth session
+var superAgent = require('superagent');
+var userAgent = superAgent.agent();
+var should = require('should');
+
 // init the test client
 var client = restify.createJsonClient({
     version: '*',
@@ -11,6 +17,19 @@ var client = restify.createJsonClient({
 // https://gist.github.com/szarsti/3348033
 // https://github.com/visionmedia/superagent#persisting-an-agent-with-cookies-ie-sessions
 
+function loginUser(agent, username, password) {
+  return function(done) {
+    agent
+      .post(client.url + '/api/v1/session/login')
+      .send({ username: username, password: password })
+      .end(onResponse);
+
+    function onResponse(err, res) {
+      //should.exist(res.headers['set-cookie']);
+      return done();
+    }
+  };
+}
 
 describe('User authentication', function() {
   // publicly accessible calls (non-authenticated)
@@ -32,14 +51,13 @@ describe('User authentication', function() {
   // Authenticate
   // Test #2
   it('success', function(done) {
-    client.post('/api/v1/session/login', {'username':'user','password':'user'}, function(err, req, res, data) {
+    userAgent.post(client.url + '/api/v1/session/login', {'username':'user','password':'user'}, function(err, req, res, data) {
+      sessionCookie = res.headers['set-coookie'][0];
       if (err) {
         throw new Error(JSON.stringify(err));
       }
       else {
-        if (data.username != 'user' || data.hashed_password == '') {
-          throw new Error('invalid response from /api/v1/session/login ' + JSON.stringify(data));
-        }
+        should.exist(res.headers['set-cookie']);
         done();
       }
     });
@@ -48,8 +66,10 @@ describe('User authentication', function() {
   // Authenticated calls
   // Test #3
   describe('get session timeout', function() {
+    userAgent = superAgent;//.agent();
+    it('should start with signin', loginUser(userAgent, 'user', 'user'));
     it('success should get an empty response', function(done) {
-      client.get('/api/v1/timeout', function(err, req, res, data) {
+      userAgent.get(client.url + '/api/v1/timeout', function(err, req, res, data) {
         if (err) {
           throw new Error(JSON.stringify(err));
         }
@@ -62,22 +82,25 @@ describe('User authentication', function() {
       });
     });
   });
-
-
-  // Test #(last) Logout
-  it('success', function(done) {
-    // logout
-    client.get('/api/v1/session/logout', function(err, req, res, data) {
-      if (err) {
-        throw new Error(JSON.stringify(err));
-      }
-      else {
-        if (JSON.stringify(data) != '{}') {
-          throw new Error('invalid response from /api/v1/session/logout ' + JSON.stringify(data));
+  // Test #4
+  describe('logout', function() {
+    before(loginUser(userAgent, 'user', 'user'));
+    it('success', function(done) {
+      // logout
+      userAgent.get(client.url + '/api/v1/session/logout', function(err, req, res, data) {
+        if (err) {
+          throw new Error(JSON.stringify(err));
         }
-        done();
-      }
+        else {
+          should.not.exist(res.headers['set-cookie']);
+          done();
+        }
+      });
     });
   });
+
+
+
 });
+
 
